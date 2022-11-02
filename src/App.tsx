98d1cb15564b77535c5000720dad4c2cfe1ebce9
unknown <Sena@ARMDFPCCIGOD830.FormacionSena.RED>
2022-11-02 17:14:09 -0500

    inicindo proyecto con tailwincss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+   <>
+   <Routes>
+
+    <Route path='*' element="Esta ruta no existe"/>
+    <Route path='/hola' element="Esta ruta si existe"/>
+    <Route path='/login' element="el formulario"/>
+   </Routes>
+
+   </>
   );
 }
 
