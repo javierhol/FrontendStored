2998e0dd9d5a8c3230f05b155f80ff217efcc9f1
soyleidy <leidysaco21@gmail.com>
2022-11-23 18:48:41 -0500

    1) Creacion de login
    - validar campos email y password
    - mostrar y ocultar contraseña
    - interface terminada
    - boton de authenticacion con google

--- src/components/AuthUser.jsx
+++ src/components/AuthUser.jsx
@@ -1,99 +1,158 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../index.css';
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope,faLock,faCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faKey, faCircleQuestion,faEye,faPhone, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { BiMusic } from "react-icons/bi";
 import * as Yup from "yup";
-import { Link} from "react-router-dom"
+import google from "../assets/img/google.png"
+import 'animate.css';
+import { Link } from "react-router-dom"
 export const AuthUser = () => {
-    let errores={}
-  return (
-   <>
-   <div className="form_Login w-1/5 ">
-    <div className="login bg-gray-100 w-full ">
-        <h1 className='text-blanck text-2xl text-center font-sans font-medium'>Iniciar sesión</h1>
 
-        <div className="descripcion">
-            <p className="title text-gray-600 text-base font-sans">
-                Ingrese sus datos para comenzar
-            </p>
-        </div>
+    const [typeInput, setTypeInput] = useState(true)
+ 
+    return (
+        <>
+            <div className="form_Login w-2/5 m-5 drop-shadow-2xl ">
+                <div className="login bg-gray-100 w-full rounded-md">
+                    <h1 className='text-black  block p-2  text-2xl text-center font-sans font-medium'>Iniciar sesión</h1>
 
-        <Formik
-        initialValues={{ email: "", password: "",toggle: false,
-        checked: [], }}
-         
-        validationSchema={
-            Yup.object({
-                email: Yup.string().email("El email no es valido").required("El campo no puede estar vacio"),
-                password: Yup.string().required("La contraseña no es valido")
-            })
-        }
-        onSubmit={(values)=>{
-            alert("Hola")
-        }}
-        >
-
-            <Form>
-
-
-               <div className="Fiel-email">
-                <div className="icons">
-                    <FontAwesomeIcon icon={faEnvelope}/>
-                </div>
-
-                <div className="email">
-                <Field  type="email" name="email" placeholder="Correo electronico"/>
-                </div>
-
-                </div>
-                <div className="error">
-                <ErrorMessage component="p" className='text-red-600' name='email' />                                                                                                                                                                  
-                </div>
-                
-                <div className="Field-password">
-                    <div className="icons">
-                        <FontAwesomeIcon icon={faLock}/>
+                    <div className="descripcion">
+                        <p className="title text-gray-600 text-base font-sans
+                        mt-6 mb-2
+                        mx-3">
+                            Ingrese sus datos para comenzar
+                        </p>
                     </div>
+                    <Formik
+                        initialValues={{
+                            email: "", password: "", toggle: false,
+                            checked: [],
+                        }}
+                        validationSchema={
+                            Yup.object({
+                                email: Yup.string().email("El email no es valido").required("El campo no puede estar vacio"),
+                                password: Yup.string().required("El campo no puede estar vacio").min(6,"Debe tener mas de 6 caracteres")
+                                ,
+                            })
+                        }
+                        onSubmit={(values) => {
+                            alert("Hola")
+                        }}
+                    >
+                        <Form>
 
-                <div className="password">
-                <Field type="password" name="password" placeholder="Contraseña"/>
-              
-                </div>
-                </div>
+                            <div className="Fiel-email bg-white flex items-center mx-2 my-1
+                           border-solid border-1 border-slate-300 rounded
+                             ">
+                                <div className="icons   py-2 px-2 text-gray-400">
+                                    <FontAwesomeIcon icon={faEnvelope} 
+                                    
+                                    className="mx-1 text-xl"/>
+                                </div>
+                                <div className="email w-full">
+                                    <Field type="email" name="email"
+                                        placeholder="Correo electronico"
+                                        className="w-full block
+                                         outline-none " />
+                                </div>
+                            </div>
+                            <div className="error">
+                                <ErrorMessage component="p" className='mx-2 block text-red-600
+                                animate__animated animate__fadeInUp ' name='email' />
+                            </div>
 
-                <div className="error">
-                <ErrorMessage component="p" className='text-red-600' name='password'/>  
-                </div>
+                            <div className="Fiel-email bg-white flex items-center mx-2 mt-6
+                           border-solid border-1 border-slate-300 rounded">
+                                <div className="icons    py-2 px-2 text-gray-400">
+                                    <FontAwesomeIcon icon={faKey}
+                                    
+                                    className="mx-1 text-xl"/>
+                                </div>
 
-                <div className="ayuda">
-                    <p>  <FontAwesomeIcon icon={faCircleQuestion}/> Ayuda</p>
-                    <Link to="/signup">
-                        ¿Olvidaste tu contraseña?
-                    </Link>
-                      </div>
+                                <div className=" w-full">
+                                    <Field type={typeInput == true?"password":"text"} name="password" placeholder="Contraseña"
+                                    className="w-full block
+                                    outline-none"
+                                    
+                                    />
 
-                <div className="button">
-                <button  type='submit'>Iniciar sesión</button>
-                </div>
+                                </div>
+                                <div className="passEye 
+                                 py-2 px-2 text-gray-400
+                                 cursor-pointer
+                                "
+                                name="eye"
+                                onClick={()=>{
+                                    setTypeInput(!typeInput)
+                                }}>
+                                    {typeInput == true ?<FontAwesomeIcon icon={faEyeSlash}
+                                    className="animate__animated animate__fadeInRight"/>:<FontAwesomeIcon icon={faEye}
+                                    className="animate__animated animate__fadeInRight"/>}
+                                    
+                                </div>
+                            </div>
 
-                <div className="permisos">
-                <Field type="checkbox" name="toggle" />
-                <Link to="/privacy">
-                Acepto terminos y condiciones
-                </Link>
-                </div>
+                            <div className="error">
+                                <ErrorMessage component="p" className='mx-2 block text-red-600
+                                animate__animated animate__fadeInUp' name='password' />
+                            </div>
 
-                <div className="ajuste-cuenta">
-                    <p>¿No tienes cuenta?</p>
-                    <Link to="/signup">
-                        <p>Crear cuenta</p>
-                    </Link>
+                            <div className="flex justify-between m-3">
+                               <Link to="ayuda">
+                               <p>  <FontAwesomeIcon icon={faCircleQuestion}
+                                
+                                className="text-[#074766] text-xl"/> Ayuda</p>
+                               </Link>
+                                <Link to="/recoveryPassword"
+                                className='text-[#0099FF] hover:underline'>
+                                    ¿Olvidaste tu contraseña?
+                                </Link>
+                            </div>
+
+                            <div className="button w-full">
+                                <button type='submit'
+                                className='bg-[#0099FF] text-white rounded-full
+                                p-1 w-5/6 block mx-auto my-3 hover:opacity-[0.85] transition'
+                                >Iniciar sesión</button>
+                            </div>
+
+                            <div className="permisos  mx-2 mt-2">
+                                <Field type="checkbox" name="toggle"
+                                className="text-xl" />
+                                <Link to="/privacy"
+                                className='mx-2  text-slate-900 hover:underline overflow-hidden
+                                '>
+                                    Acepto términos y condiciones
+
+                                </Link>
+                            </div>
+
+                            <div className="ajuste-cuenta   flex justify-start mt-4 mx-2 mb-2
+                            ">
+                                <p>¿No tienes cuenta?</p>
+                                <Link to="/signup">
+                                    <p className='text-[#0099FF] mb-3 ml-3'>Crear cuenta</p>
+                                </Link>
+                            </div>
+                            <div className="countCuenda">
+                                <div className="authGoogle bg-slate-200
+                                p-2 m-2 flex items-center justify-center rounded">
+                                    <div className="count-g">
+                                    <img src={google}
+                                    className="w-9"/>
+                                    </div>
+                                    <div className="p ml-1">
+                                    Iniciar sesión con Google
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="m-2 p-1"></div>
+                        </Form>
+                    </Formik>
                 </div>
-            </Form>
-        </Formik>
-    </div>
-   </div>
-   </>
-  )
+            </div>
+        </>
+    )
 }
