fcba6738e9282c11b7776b7908bcd7eff356d5c7
danielsansp <daniel1008ospina@gmail.com>
2022-11-29 19:38:54 -0500

    api auth server stored

--- src/apis/ApiData.jsx
+++ src/apis/ApiData.jsx
@@ -1,3 +1,3 @@
 import axios from "axios"
 var getUrl = "http://localhost:3002"
-export const PostDataUser = async (postDataUser) => await axios.post( `${getUrl}/login`, { postDataUser})
+export const PostDataUser = async (postDataUser) => await axios.post(`${getUrl}/login`, { postDataUser})
