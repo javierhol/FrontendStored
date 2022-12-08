82e751765e5339ac91a396ea0e87ad4fed64bd7d
fernanda11232 <fernandahenao868@gmail.com>
2022-12-07 21:27:27 -0500

    Actualizando Header1

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,10 +12,10 @@ export const Search=() =>{
     <>    
      
       <Formik>
-      <Form className="xl:w-2/4 2xl:w-1/4 2xl:mr-16">
-      <div className="border-2 border-[#BFDDD8] rounded-full py-1 flex justify-center sm:w-44 sm:my-2 sm:mt-3 md:w-64 md:mx-5 lg:w- xl:w-96">
-        <Field type="search" placeholder="¿Que deseas buscar?" className="form-control-lg  outline-0 sm:w-32 sm:text-xs md:w-52 lg:w-72 xl:w-full xl:mx-5"/>
-        <FontAwesomeIcon className="fa-search color-[#BCBCBC] xl:mr-5" icon={faMagnifyingGlass}/>
+      <Form className="xl:w-2/5 2xl:w-1/4 2xl:mr-16">
+      <div className="border-2 border-gray-300 rounded-full py-1 flex justify-center sm:w-40 sm:my-2 sm:mt-3 md:w-60 md:mx-5 xl:w-96 xl:p-1 xl:mt-2 2xl:p-2">
+        <Field type="search" placeholder="¿Que deseas buscar?" className="form-control-lg  outline-0 sm:w-32 sm:text-xs md:w-52  lg:mx-2 lg:w-72 xl:w-96 xl:text-sm xl:mx-2"/>
+        <FontAwesomeIcon className="fa-search text-gray-300  lg:mr-2 xl:mr-2 xl:my-1" icon={faMagnifyingGlass}/>
       </div>
       </Form>
     </Formik>
