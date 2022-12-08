f6405dad901f6c6e3686022bab668dd04cf7abec
danielsansp <daniel1008ospina@gmail.com>
2022-12-08 11:08:11 -0500

    source

--- src/components/AuthUser.jsx
+++ src/components/AuthUser.jsx
@@ -8,7 +8,8 @@ import {
   faCircleQuestion,
   faEye,
   faEyeSlash,
-  faAngleLeft
+  faAngleLeft,
+  faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
@@ -49,19 +50,18 @@ export const AuthUser = () => {
   return (
     <>
       <ToastContainer />
-      <div className="form_Login w-72 mx-auto sm:w-96    my-9 drop-shadow-2xl ">
-        <div className="login bg-white w-full rounded-md">
+      <div className="form_Login  mx-auto rounded-md border form w-[50rem]  my-9 drop-shadow-2xl ">
+        <div className="login bg-white relative rounded-md animate__animated animate__fadeInDown">
           <div className="flex item-center justify-center">
-
             <div className=" absolute p-2 left-0 text-3xl text-gray-600 ">
               <Link to="/home">
-                <FontAwesomeIcon icon={faAngleLeft}/>
+                <FontAwesomeIcon icon={faAngleLeft} />
               </Link>
             </div>
             <div className="">
-              <h1 className="text-black text-gray-600  block p-2  text-2xl text-center font-sans font-medium">
-            Iniciar sesión
-          </h1>
+              <h1 className=" text-gray-600  block p-2  text-2xl text-center font-sans font-medium">
+                Iniciar sesión
+              </h1>
             </div>
           </div>
 
@@ -74,189 +74,220 @@ export const AuthUser = () => {
               Ingrese sus datos para comenzar
             </p>
           </div>
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            validationSchema={Yup.object({
-              email: Yup.string()
-                .email("El email no es valido")
-                .required("El campo no puede estar vacio"),
-              password: Yup.string().required("El campo no puede estar vacio"),
-            })}
-            onSubmit={async (values) => {
-              let response = await getPostLogin(values);
-              console.log(response);
-              if (response.status === 200) {
-                toast.success("Cargando...", {
-                  position: toast.POSITION.TOP_RIGHT,
-                  theme: "dark",
-                });
-                let getData = response.data;
-                localStorage.setItem("secure_token", getData.token);
-                localStorage.setItem("auth_cuenta", getData.auth);
-                localStorage.setItem("response_auth", getData.message);
-                localStorage.setItem("perfil_rol", getData.rol);
-                setSpiner(!spiner);
-                window.location.href = "/dasboard";
-              }
-              if (response.response.status === 400) {
-                toast.error("Este usuario no existe", {
-                  position: toast.POSITION.TOP_RIGHT,
-                  theme: "dark",
-                });
-                await setSpiner(!spiner);
-              } else if (response.response.status === 401) {
-                toast.warning("La contraseña es incorrecta", {
-                  position: toast.POSITION.TOP_RIGHT,
-                  theme: "dark",
-                });
-                await setSpiner(!spiner);
-              }
-            }}
-          >
-            <Form>
-              <div
-                className="Fiel-email border bg-white flex items-center mx-2 my-1
+          <div className="grid grid-cols-2 ">
+            <div className="">
+              <Formik
+                initialValues={{
+                  email: "",
+                  password: "",
+                }}
+                validationSchema={Yup.object({
+                  email: Yup.string()
+                    .email("El email no es valido")
+                    .required("El campo no puede estar vacio"),
+                  password: Yup.string().required(
+                    "El campo no puede estar vacio"
+                  ),
+                })}
+                onSubmit={async (values) => {
+                  let response = await getPostLogin(values);
+                  console.log(response);
+                  if (response.status === 200) {
+                    toast.success("Cargando...", {
+                      position: toast.POSITION.TOP_RIGHT,
+                      theme: "dark",
+                    });
+                    let getData = response.data;
+                    localStorage.setItem("secure_token", getData.token);
+                    localStorage.setItem("auth_cuenta", getData.auth);
+                    localStorage.setItem("response_auth", getData.message);
+                    localStorage.setItem("perfil_rol", getData.rol);
+                    setSpiner(!spiner);
+                    window.location.href = "/dasboard";
+                  }
+                  if (response.response.status === 400) {
+                    toast.error("Este usuario no existe", {
+                      position: toast.POSITION.TOP_RIGHT,
+                      theme: "dark",
+                    });
+                    await setSpiner(!spiner);
+                  } else if (response.response.status === 401) {
+                    toast.warning("La contraseña es incorrecta", {
+                      position: toast.POSITION.TOP_RIGHT,
+                      theme: "dark",
+                    });
+                    await setSpiner(!spiner);
+                  }
+                }}
+              >
+                <Form>
+                  <div
+                    className="Fiel-email border bg-white flex items-center mx-2 my-1
                            border-solid border-1 border-slate-300 rounded transition-200
                              "
-              >
-                <div className="icons   py-2 px-2 text-gray-400">
-                  <FontAwesomeIcon icon={faEnvelope} className="mx-1 text-xl" />
-                </div>
-                <div className="email w-full">
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Correo electronico"
-                    className="w-full block
+                  >
+                    <div className="icons   py-2 px-2 text-gray-400">
+                      <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className="mx-1 text-xl"
+                      />
+                    </div>
+                    <div className="email w-full">
+                      <Field
+                        type="email"
+                        name="email"
+                        placeholder="Correo electronico"
+                        className="w-full block
                                          outline-none "
-                  />
-                </div>
-              </div>
-              <div className="error">
-                <ErrorMessage
-                  component="p"
-                  className="mx-2 block text-red-600
+                      />
+                    </div>
+                  </div>
+                  <div className="error">
+                    <ErrorMessage
+                      component="p"
+                      className="mx-2 block text-red-600
                                 animate__animated animate__fadeInUp "
-                  name="email"
-                  // validate={validateEmail}
-                />
-              </div>
+                      name="email"
+                      // validate={validateEmail}
+                    />
+                  </div>
 
-              <div
-                className="Fiel-password border bg-white flex items-center mx-2 mt-6
+                  <div
+                    className="Fiel-password border bg-white flex items-center mx-2 mt-6
                            border-solid border-1 border-slate-300 rounded"
-              >
-                <div className="icons    py-2 px-2 text-gray-400">
-                  <FontAwesomeIcon icon={faKey} className="mx-1 text-xl" />
-                </div>
+                  >
+                    <div className="icons    py-2 px-2 text-gray-400">
+                      <FontAwesomeIcon icon={faKey} className="mx-1 text-xl" />
+                    </div>
 
-                <div className=" w-full">
-                  <Field
-                    type={typeInput === true ? "password" : "text"}
-                    name="password"
-                    placeholder="Contraseña"
-                    className="w-full block 
+                    <div className=" w-full">
+                      <Field
+                        type={typeInput === true ? "password" : "text"}
+                        name="password"
+                        placeholder="Contraseña"
+                        className="w-full block 
                                     outline-none bg-white"
-                  />
-                </div>
-                <div
-                  className="passEye 
+                      />
+                    </div>
+                    <div
+                      className="passEye 
                                  py-2 px-2 text-gray-400
                                  cursor-pointer
                                 "
-                  name="eye"
-                  onClick={() => {
-                    setTypeInput(!typeInput);
-                  }}
-                >
-                  {typeInput === true ? (
-                    <FontAwesomeIcon
-                      icon={faEyeSlash}
-                      className="animate__animated animate__fadeInRight"
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faEye}
-                      className="animate__animated animate__fadeInRight"
-                    />
-                  )}
-                </div>
-              </div>
+                      name="eye"
+                      onClick={() => {
+                        setTypeInput(!typeInput);
+                      }}
+                    >
+                      {typeInput === true ? (
+                        <FontAwesomeIcon
+                          icon={faEyeSlash}
+                          className="animate__animated animate__fadeInRight"
+                        />
+                      ) : (
+                        <FontAwesomeIcon
+                          icon={faEye}
+                          className="animate__animated animate__fadeInRight"
+                        />
+                      )}
+                    </div>
+                  </div>
 
-              <div className="error">
-                <ErrorMessage
-                  component="p"
-                  className="mx-2 block text-red-600
+                  <div className="error">
+                    <ErrorMessage
+                      component="p"
+                      className="mx-2 block text-red-600
                                 animate__animated animate__fadeInUp"
-                  name="password"
-                />
-              </div>
+                      name="password"
+                    />
+                  </div>
 
-              <div className="flex justify-between m-3">
-                <Link to="ayuda">
-                  <p>
-                    {" "}
-                    <FontAwesomeIcon
-                      icon={faCircleQuestion}
-                      className="text-[#074766] text-xl"
-                    />{" "}
-                    Ayuda
-                  </p>
-                </Link>
-                <Link
-                  to="/recovery+password/identify"
-                  className="text-[#0099FF] hover:underline"
-                >
-                  ¿Olvidaste tu contraseña?
-                </Link>
-              </div>
+                  <div className="flex justify-between m-3">
+                    <Link to="ayuda">
+                      <p>
+                        {" "}
+                        <FontAwesomeIcon
+                          icon={faCircleQuestion}
+                          className="text-[#074766] text-xl"
+                        />{" "}
+                        Ayuda
+                      </p>
+                    </Link>
+                    <Link
+                      to="/recovery+password/identify"
+                      className="text-[#0099FF] hover:underline"
+                    >
+                      ¿Olvidaste tu contraseña?
+                    </Link>
+                  </div>
 
-              <div className="button w-full relative">
-                <button
-                  type="submit"
-                  className="bg-[#0099FF] text-white rounded-full relative
+                  <div className="button w-full relative">
+                    <button
+                      type="submit"
+                      className="bg-[#0099FF] text-white rounded-full relative
                                 p-1 py-1.5 w-5/6 mx-auto my-3 hover:opacity-[0.85] transition
                                 h-9 flex justify-center"
-                >
-                  {spiner === true ? (
-                    <span onClick={() => setSpiner(!spiner)}>
-                      Iniciar sesión
-                    </span>
-                  ) : (
-                    <div className="centh">
-                      <div className="lds-ellipsis">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                      </div>
-                    </div>
-                  )}
-                </button>
-              </div>
+                    >
+                      {spiner === true ? (
+                        <span onClick={() => setSpiner(!spiner)}>
+                          Iniciar sesión
+                        </span>
+                      ) : (
+                        <div className="centh">
+                          <div className="lds-ellipsis">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                          </div>
+                        </div>
+                      )}
+                    </button>
+                  </div>
 
-              <div className="permisos  mx-2 mt-2">
-                <Field type="checkbox" name="toggle" className="text-xl" />
-                <Link
-                  to="/privacy"
-                  className="mx-2  text-slate-900 hover:underline overflow-hidden
+                  <div className="permisos  mx-2 mt-2">
+                    <Field type="checkbox" name="toggle" className="text-xl" />
+                    <Link
+                      to="/privacy"
+                      className="mx-2  text-slate-900 hover:underline overflow-hidden
                                 "
-                >
-                  Acepto términos y condiciones
-                </Link>
-              </div>
+                    >
+                      Acepto términos y condiciones
+                    </Link>
+                  </div>
 
-              <div
-                className="ajuste-cuenta   flex justify-start mt-4 mx-2 mb-2
+                  <div
+                    className="ajuste-cuenta   flex justify-start mt-4 mx-2 mb-2
                             "
+                  >
+                    <p>¿No tienes cuenta?</p>
+                    <Link to="/signup">
+                      <p className="text-[#0099FF] mb-3 ml-3">Crear cuenta</p>
+                    </Link>
+                  </div>
+                </Form>
+              </Formik>
+            </div>
+
+            <div className="entry mx-1 rounded-md">
+              <video
+                src="https://res.cloudinary.com/dkqp3wkbi/video/upload/v1670421680/animacion_media_fawwzt.mp4"
+                autoPlay
+                playinline
+                loop
+                muted
+                preload="auto"
+              ></video>
+            </div>
+          </div>
+          <div className="op-goo flex flex-rows justify-center items-center ">
+            <div className=" flex  justify-center items-center">
+              <div
+                className="authGoogle bg-gray-200 relative
+                                p-1.5 m-2 flex items-center justify-center rounded-full"
               >
-                <p>¿No tienes cuenta?</p>
-                <Link to="/signup">
-                  <p className="text-[#0099FF] mb-3 ml-3">Crear cuenta</p>
-                </Link>
+                <span className="mx-1">puedes usar stored con otra cuenta</span>
+                <span> </span>
               </div>
               <div className="countCuenda cursor-pointer">
                 <div
@@ -279,11 +310,8 @@ export const AuthUser = () => {
                   />
                 </div>
               </div>
-              <div className="m-2 p-1"></div>
-            </Form>
-          </Formik>
-          
-          <video  src="https://res.cloudinary.com/dkqp3wkbi/video/upload/v1670421680/animacion_media_fawwzt.mp4" autoPlay  playsInline></video>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,30 +9,32 @@ export const Header=() =>{
 
         <>
         
-        <header className="flex bg-white px-5">
-    <h1 className="text-[#1876F2] font-medium text-3xl p-4">Stored</h1>
-    <nav>
-      <ul className="flex">
-        <li className="nav-item py-5 px-6">
-          <a className="nav-link hover:text-gray-700 focus:text-gray-700 p-0" href="#">Servicios</a>
-        </li>
-        <li className="nav-item px-2 py-5">
-          <a className="nav-link hover:text-gray-700 focus:text-gray-700 p-0" href="#">Ayuda</a>
-        </li>
-        <li className="nav-item px-2 py-5">
-          <a className="nav-link hover:text-gray-700 focus:text-gray-700 p-0" href="#">Contactanos</a>
-        </li>
+      <header className="flex bg-white w-full h-14 border-2 border-gray-100 border-b-gray-100">
+    <h1 className="text-[#1876F2] font-medium  sm:m-2 sm:text-xl hover:text-decoration md:ml-5 lg:mr-3 lg:ml-5 lg:m-2 lg:text-2xl xl:text-2xl xl:mt-3 xl:ml-10 2xl:text-3xl 2xl:mt-1 2xl:my-5 2xl:ml-14">Stored</h1>
+    <nav className="flex w-full lg:justify-around">
+      <ul className="flex sm:text-xs sm:py-4 lg:w-60 lg:text-base xl:w-80 xl:justify-around 2xl:w-1/4 2xl:mb-3 2xl:justify-between ">
+         <Link to="/" className="nav-item px-2  inline-block relative hover:underline hover:underline-offset-4 decoration-[#1876F2] origin-bottom-right">
+            <span className="nav-linktransition ">Servicios</span>
+          </Link>
+          <Link to="/" className="nav-item px-2 inline-block relative hover:underline hover:underline-offset-4 decoration-[#1876F2] origin-bottom-right">
+            <span className="nav-link">Ayuda</span>
+            </Link>
+            <Link to="/" className="nav-item px-2 inline-block relative hover:underline hover:underline-offset-4 decoration-[#1876F2] hover:bg.blue-800 origin-bottom-right">
+              <span className="nav-link ">Contactanos</span>
+            </Link>
+      </ul>
        <Search/>
-        <div className="flex py-4">
-          <li className="nav-item px-6 ">
-            <a className="nav-link  hover:text-gray-700 focus:text-gray-700 px-5" href="/login">Iniciar sesión</a>
-            <button type="button" className="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded-full hover:bg-blue-600 hover:text-white focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Crear cuenta</button>
+        <div>
+           <li className="nav-item sm:m-2 list-none lg:m-1 lg:w-60 xl:w-72 2xl:w-96">
+            <Link to="/" className="nav-item py-2 ">
+              <span className="nav-link hover:text-[#1876F2]  focus:text-gray-700 sm:text-xs lg:mr-3 lg:text-base xl:text-base 2xl:mr-0">Iniciar sesión</span>
+            </Link>
+                <Link to="signup" className="inline-block px-3 py-1 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight rounded-full hover:bg-blue-600 hover:text-white focus:outline-none focus:ring-0 transition duration-150 ease-in-out sm:text-xs sm:ml-2 sm:mt-1 lg:ml-1 lg:text-base lg:py-0 lg:mt-2 xl:text-base xl:ml-5 2xl:ml-16">
+                  Crear cuenta</Link>
           </li>
         </div>
-      </ul>
     </nav>
   </header>
-
         </>
     )
   }
